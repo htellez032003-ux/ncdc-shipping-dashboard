d10e1977e5db61602fa9353e0d9be093eb255950
htellez032003-ux <htellez032003@gmail.com>
2025-11-04 08:55:24 -0500

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('ncdc-cache').then(cache => cache.addAll(['/','/index.html','/styles.css','/script.js'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
-});
