0cf1c948dfc69de76cea4700d1ccefa678aee128
htellez032003-ux <htellez032003@gmail.com>
2025-11-11 08:31:09 -0500

    Create shippers.js

---
+++ js/shippers.js
@@ -0,0 +1,7 @@
+// js/shippers.js
+// just to avoid 404 for now – you can paste your full shipper list later
+window.NCDC_SHIPPERS = [
+  { code: "FXB", name: "FEDEX" },
+  { code: "WCA", name: "WCA" },
+  { code: "MAC", name: "MACY'S (MMC)" }
+];
