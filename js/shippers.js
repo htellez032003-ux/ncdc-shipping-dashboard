cfba16c396745296ce65f342f40555f98181a8bc
htellez032003-ux <htellez032003@gmail.com>
2025-11-11 11:02:24 -0500

    Delete js directory

--- js/shippers.js
+++
@@ -1,7 +0,0 @@
-// js/shippers.js
-// just to avoid 404 for now – you can paste your full shipper list later
-window.NCDC_SHIPPERS = [
-  { code: "FXB", name: "FEDEX" },
-  { code: "WCA", name: "WCA" },
-  { code: "MAC", name: "MACY'S (MMC)" }
-];
